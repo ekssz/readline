e4f6635ab0e2a902550d763eee9b4f3388b295f5
ekssz <81739569+ekssz@users.noreply.github.com>
2023-03-06 14:16:03 +0200

    Add files via upload

---
+++ StringOperations.java
@@ -0,0 +1,86 @@
+import java.util.Scanner;
+
+// імпорт утиліти для виконання індивідуального завдання.
+/* 
+всього він має 4 методи,
+next 
+nextLine 
+nextInt 
+nextDouble 
+*/
+public class StringOperations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*
+		 * 
+		 * System.out.println("Hello".concat(" world")); // concat використовує оператор
+		 * +, обєднує два або більше рядків.
+		 * 
+		 * String a = "StringOperations"; String b = new String("StringOperations");
+		 * String c = a; String d = "stringoperations";
+		 * 
+		 * System.out.println("a.equals(b) = " + a.equals(b));
+		 * System.out.println("b.equals(a) = " + b.equals(b));
+		 * System.out.println("a.equals(c) = " + a.equals(c));
+		 * System.out.println("a.equals(d) = " + a.equals(d));
+		 * System.out.println("a.equalsIgnoreCase(d) = " + a.equals(b)); // порівнювання
+		 * рядків, для перевірки рівності рядків використовується // equalsIgnoreCase
+		 * 
+		 * 
+		 */
+
+		/*
+		 * 
+		 * System.out.println("Індивідуальне завдання");
+		 * 
+		 * String f = "Kliuka"; String g = new String("Kovalchuk"); String h = f; String
+		 * j = "kliuka";
+		 * 
+		 * System.out.println("f.equals(g) = " + f.equals(g));
+		 * System.out.println("g.equals(f) = " + g.equals(g));
+		 * System.out.println("f.equals(h) = " + f.equals(h));
+		 * System.out.println("f.equals(j) = " + f.equals(j));
+		 * System.out.println("f.equalsIgnoreCase(j) = " + f.equals(g)); //
+		 * індивідуальне завдання
+		 * 
+		 */
+
+		/*
+		 * Ця програма спочатку виводить повідомлення про введення прізвища з
+		 * клавіатури, після введення програма порівнює введене прізвище з іншими
+		 * рядками за допомогою методі equals і equalsIgnoreCase.
+		 */
+
+		/*
+		 * Scanner scanner = new Scanner(System.in);
+		 * 
+		 * System.out.print("Введіть прізвище: "); String f = scanner.nextLine();
+		 * 
+		 * System.out.print("Введіть прізвище сусіда:"); String g = scanner.nextLine();
+		 * 
+		 * String h = f; String j = "kliuka"; System.out.println("f.equals(g) = " +
+		 * f.equals(g)); System.out.println("g.equals(f) = " + g.equals(g));
+		 * System.out.println("f.equals(h) = " + f.equals(h));
+		 * System.out.println("f.equals(j) = " + f.equals(j));
+		 * System.out.println("f.equalsIgnoreCase(j) = " + f.equals(g));
+		 */
+
+		/*
+		 * Ця програма використовує сканер для зчитування інформації яку надає
+		 * користувач за допомогою методу nextLine, рядок що був зчитаний.
+		 */
+
+		/*
+		 * String input; // змінна для збереження зчитаного рядка.
+		 * 
+		 * do { System.out.print("Введіть рядок"); input = scanner.nextLine();
+		 * System.out.println(input); } while (!input.equalsIgnoreCase("Stop"));
+		 * 
+		 * System.out.println("stop");
+		 */
+		
+	}
+
+}
